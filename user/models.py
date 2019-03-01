aa0db4bf6102bb9f8cfd5711b80779adb78ce54f
chendingbing <chendingbing@chinamcloud.com>
2019-03-01 18:37:54 +0800

    test

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=20,null=False)
+    email = models.EmailField()
+    mobile = models.CharField(max_length=16)
+    create_time = models.DateTimeField(auto_now=True)
+    password = models.CharField(max_length=100)
+    friend_id = models.IntegerField()
+
+    class Meta:
+        unique_together = ('email', 'mobile')
